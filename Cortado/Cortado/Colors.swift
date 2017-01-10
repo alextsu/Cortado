2cba6880b714edcd8e613a556ded68d2a073ee35
alextsu <alextsu@gmail.com>
2017-01-09 16:44:23 -0800

    First

---
+++ Cortado/Cortado/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  Cortado
+//
+//  Created by Alexander Tsu on 1/9/17.
+//  Copyright © 2017 Alexander Tsu. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+
+let cortadoNavy = UIColor(red: 48/255, green: 57/255, blue: 83/255, alpha: 1)
+let cortadoGreen = UIColor(red: 169/255, green: 193/255, blue: 191/255, alpha: 1)
+let cortadoDarkBrown = UIColor(red: 87/255, green: 63/255, blue: 76/255, alpha: 1)
+let cortadoLightBrown = UIColor(red: 174/255, green: 129/255, blue: 130/255, alpha: 1)
+let cortadoLightBlue = UIColor(red: 154/255, green: 199/255, blue: 221/255, alpha: 1)
---
+++ Cortado/Cortado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cortado
+//
+//  Created by Alexander Tsu on 1/9/17.
+//  Copyright © 2017 Alexander Tsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
